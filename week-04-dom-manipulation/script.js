57a3e980751f30c22d1494516d2a0555f61fc228
RCJonesIntrotoWebDev <98345898+RCJonesIntrotoWebDev@users.noreply.github.com>
2022-02-16 12:51:24 -0500

    Update script.js

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -17,4 +17,79 @@ function fillOutputs() {
   const outputDiv = document.getElementById('output');
   // Make the output container element visible
   outputDiv.style.display = 'block';
+  
+    // Get the greeting input element
+  const greetingOneInput = document.getElementById('adjective-one-input');
+  // Get the text from the input element
+  const greetingOne = greetingOneInput.value;
+
+  // Get the greeting output element
+  const greetingOneOutput = document.getElementById('greeting-one-output');
+  // Set the text of the input element
+  greetingOneOutput.innerText = greetingOne;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
+  
+    // Get the adjective input element
+  const adjectiveTwoInput = document.getElementById('adjective-two-input');
+  // Get the text from the input element
+  const adjectiveTwo = adjectiveTwoInput.value;
+
+  // Get the adjective output element
+  const adjectiveTwoOutput = document.getElementById('adjective-two-output');
+  // Set the text of the input element
+  adjectiveTwoOutput.innerText = adjectiveTwo;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
+  
+      // Get the adjective input element
+  const adjectiveOneInput = document.getElementById('adjective-one-input');
+  // Get the text from the input element
+  const adjectiveOne = adjectiveOneInput.value;
+
+  // Get the adjective output element
+  const adjectiveOneOutput = document.getElementById('adjective-one-output');
+  // Set the text of the input element
+  adjectiveOneOutput.innerText = adjectiveOne;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
+  
+      // Get the color input element
+  const colorOneInput = document.getElementById('color-one-input');
+  // Get the text from the input element
+  const colorOne = colorOneInput.value;
+
+  // Get the color output element
+  const colorOneOutput = document.getElementById('color-one-output');
+  // Set the text of the input element
+  colorOneOutput.innerText = colorOne;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
+  
+      // Get the skechers input element
+  const skechersOneInput = document.getElementById('skechers-one-input');
+  // Get the text from the input element
+  const skechersOne = skechersOneInput.value;
+
+  // Get the skechers output element
+  const skechersOneOutput = document.getElementById('skechers-one-output');
+  // Set the text of the input element
+  skechersOneOutput.innerText = skechersOne;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
 }
