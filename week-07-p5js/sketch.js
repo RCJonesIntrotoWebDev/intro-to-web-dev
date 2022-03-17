36d03bdedbe8468448b1cc9481ce6a8e2d5aff66
RCJonesIntrotoWebDev <98345898+RCJonesIntrotoWebDev@users.noreply.github.com>
2022-03-17 14:50:25 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -2,9 +2,6 @@ function setup() {
   createCanvas(100, 100);
 }
 
-function draw() {
-  background(100);
-  stroke(255);
   
   for (let lineX = 30; lineX <= 270; lineX += 30) {
     line(lineX, 0, lineX, height);
