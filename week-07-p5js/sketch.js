66885bce428db63d4a1de6c6e8021cfd76cb0a1e
RCJonesIntrotoWebDev <98345898+RCJonesIntrotoWebDev@users.noreply.github.com>
2022-03-16 22:28:06 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,15 +1,7 @@
 function setup() {
   createCanvas(300, 300);
-  background(32);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    circle(mouseX, mouseY, 25);
-  }
-
-    for (let lineX = 75; lineX <= 143; lineX += 132) {
-    line(lineX, 0, lineX, height);
-  }
-}
-
+  background(400)
+  star(300,300,400)
