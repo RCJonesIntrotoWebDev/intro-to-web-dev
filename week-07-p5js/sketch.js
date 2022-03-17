8285eff7be57c5c697227159f379e1ae00fddebb
RCJonesIntrotoWebDev <98345898+RCJonesIntrotoWebDev@users.noreply.github.com>
2022-03-17 14:45:46 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -4,5 +4,14 @@ function setup() {
 
 function draw() {
   background(100);
+  stroke(255);
+  
+  for (let lineX = 30; lineX <= 270; lineX += 30) {
+    line(lineX, 0, lineX, height);
+  }
+}
+
+function draw() {
+  background(100);
   circle(mouseX, mouseY, 100);
 }
