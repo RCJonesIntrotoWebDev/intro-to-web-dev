9f28cd832370ccb74e115f893fb80086c21088fe
RCJonesIntrotoWebDev <98345898+RCJonesIntrotoWebDev@users.noreply.github.com>
2022-03-16 22:20:49 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -7,3 +7,9 @@ function draw() {
   if (mouseIsPressed) {
     circle(mouseX, mouseY, 25);
   }
+
+    for (let lineX = 75; lineX <= 143; lineX += 132) {
+    line(lineX, 0, lineX, height);
+  }
+}
+
