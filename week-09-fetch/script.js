30cf0b7b55cfd280c21d391cd7dfef5be281487b
RCJonesIntrotoWebDev <98345898+RCJonesIntrotoWebDev@users.noreply.github.com>
2022-03-30 14:32:49 -0400

    Update script.js

--- week-09-fetch/script.js
+++ week-09-fetch/script.js
@@ -1,7 +1,7 @@
 // Loads content for a Wikipedia article and displays it in the page.
 async function loadContent() {
   const pageId = 'Cat';
-  const response = await fetch();
+  const response = await fetch(IMG_5734.ogg);
   const json = await response.json();
   const article = json.parse;
 
